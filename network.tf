42ac2ef1a2512c023e1b119b2e04eec4573880f2
Carlos Doki <carlosdoki@gmail.com>
2023-01-31 13:23:37 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,7 @@
 module "network" {
   source  = "app.terraform.io/VortexLab/network/azurerm"
   version = "5.1.0"
-  # insert required variables here
+
+  resource_group_name = "carlosdoki"
+  use_for_each = 1
 }
